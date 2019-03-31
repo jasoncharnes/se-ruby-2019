a97e960df6d367a7dab60d0e4265a0509378e178
Jason Charnes <jason@thecharnes.com>
2019-03-31 12:24:38 -0500

    Adds image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/styles.css',
+  entry: "./src/styles.css",
   mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
+          fallback: "style-loader",
           use: [
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader',
-          ],
-        }),
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            "postcss-loader"
+          ]
+        })
       },
-    ],
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader"
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader"
+      }
+    ]
   },
   plugins: [
-    new ExtractTextPlugin('styles.css', {
-      disable: process.env.NODE_ENV === 'development',
+    new ExtractTextPlugin("styles.css", {
+      disable: process.env.NODE_ENV === "development"
     }),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-  ],
-}
+      filename: "index.html",
+      template: "src/index.html"
+    })
+  ]
+};
